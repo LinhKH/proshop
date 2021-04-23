427f2c7a7eca868420462998f17850af454932c2
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2021-04-23 22:45:43 +0700

    section 2: Starting the front end

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Form, FormControl, Button, Container } from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+
+                    <LinkContainer to='/'>
+                        <Navbar.Brand href="#home">ProShop</Navbar.Brand>
+                    </LinkContainer>
+
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                        <Form inline>
+                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+
+            </Navbar>
+        </div>
+    )
+}
+
+export default Header
