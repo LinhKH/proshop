b595f52e6a5d6402eb7c9cfa82e9c5b2a2ccf8fc
Kieu Hoa Linh <l_kieu@vision-net.co.jp>
2021-04-26 20:44:30 +0700

    section 3: serving and fetching data from express finish

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({match}) => {
-    const product = products.find( p => p._id === +match.params.id)
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${match.params.id}`);
+            setProduct(data)
+        }
+        fetchProduct();
+    }, [])
 
     return (
         <>
@@ -22,7 +30,7 @@ const ProductScreen = ({match}) => {
                             <h3>{product.name}</h3>
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+                            <Rating value={product.rating ? product.rating : 0} text={`${product.numReviews} reviews`} />
                         </ListGroup.Item>
                         <ListGroup.Item>
                             Price: ${product.price}
